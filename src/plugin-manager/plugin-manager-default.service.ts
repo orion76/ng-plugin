2bf770748e1af1e605eed4605d9aa6f6105008c3
Paul Glazkow <gpb@hos247.com>
2025-04-01 16:13:26 +0400

    fix

--- src/plugin-manager/plugin-manager-default.service.ts
+++ src/plugin-manager/plugin-manager-default.service.ts
@@ -1,6 +1,7 @@
 import { inject } from '@angular/core';
-import { IPlugin, IPluginBuilder, IPluginDiscovery, PluginManagerBase } from '@orion76/plugin;
+
 import { PLUGIN_BUILDER, PLUGIN_DISCOVERY, PLUGIN_TYPE } from '../injection-tokens';
+import { IPlugin, PluginManagerBase, IPluginBuilder, IPluginDiscovery } from '@orion76/plugin';
 
 
 export class PluginManagerDefault<P extends IPlugin = IPlugin> extends PluginManagerBase<P> {
